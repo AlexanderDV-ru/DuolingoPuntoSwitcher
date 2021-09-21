2f1722119e16d8ffc9f158e0d32fbc58cacb9f8b
AlexanderDV <AlexanderDV.ru@gmail.com>
2021-09-21 16:24:40 +0500

    Update duolingoPuntoSwitcher.js
    v0.0.3

--- duolingoPuntoSwitcher.js
+++ duolingoPuntoSwitcher.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         DuolingoPuntoSwitcher
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
-// @description  changes letters to russian if it needs. Note: please and sentence by space before press enter and don't use upper case if you don't want to have small errors
+// @version      0.0.3
+// @description  changes letters to russian if it needs in duolingo. Note: for fully correct script work end sentences by space
 // @author       AlexanderDV-ru
 // @match        https://www.duolingo.com/*
 // @icon         https://www.duolingo.com/
@@ -12,25 +12,27 @@
 setInterval(function() {
 
     var map = {
-        ru: [ "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю" ],
-        en: [ "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", "." ]
+        ru: ["йцукенгшщзхъфывапролджэячсмитьбюё","ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ"],
+        en: ["qwertyuiop[]asdfghjkl;'zxcvbnm,.`","QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>~"]
     };
 
-    var ru,en
-    let areas=document.getElementsByTagName("textarea"),area
-    for(let i=0;i<areas.length;i++)
-        if(areas[i].getAttribute("data-test")=="challenge-translate-input")
-            area=areas[i]
-    let spans=document.getElementsByTagName("span"),span
-    for(let i=0;i<spans.length;i++)
-        if(spans[i].getAttribute("data-test")=="hint-token")
-            span=spans[i]
+    var elems=document.getElementsByTagName("*"),area,span,hasRu,ru,en
+    for(let i=0;i<elems.length;i++)
+        switch(elems[i].getAttribute("data-test")){
+            case "challenge-translate-input":
+            case "challenge-text-input":
+                area=elems[i]
+                break
+            case "hint-token":
+            case "hint-sentence":
+                span=elems[i]
+                for(let l of map.ru[0]+map.ru[1])
+                    if(span.innerText.indexOf(l)!=-1)
+                        hasRu=true
+                break
+        }
     if(!area||!span)
         return
-    let hasRu
-    for(let l of map.ru)
-        if(span.innerText.indexOf(l)!=-1)
-            hasRu=true
     if(hasRu)
         en=area
     else ru=area
@@ -47,11 +49,11 @@ setInterval(function() {
     function setInputText ( e ) {
         var k = e.key;
         let curMap=map[this.__lang[1]]
-        let i=curMap.indexOf(k),lc
-        if(i==-1&&curMap.indexOf(k.toLowerCase())!=-1)
+        let i=curMap[0].indexOf(k),lc=0
+        if(i==-1&&curMap[1].indexOf(k)!=-1)
         {
-            i=curMap.indexOf(k.toLowerCase())
-            lc=true
+            i=curMap[1].indexOf(k)
+            lc=1
         }
         let start = this.selectionStart, end = this.selectionEnd
         console.log(i,lc,k,e,start,end)
@@ -63,10 +65,10 @@ setInterval(function() {
         if (i !== -1 ) {
             //e.preventDefault();
             let th=this
-            //th.setRangeText(lc?map[th.__lang[0]][i].toUpperCase():map[th.__lang[0]][i], start, end);
+            //th.setRangeText(map[th.__lang[0]][lc][i], start, end);
             setTimeout(()=>{
                 //th.setRangeText("", end, end+1 );
-                th.setRangeText(lc?map[th.__lang[0]][i].toUpperCase():map[th.__lang[0]][i], start, end+1);
+                th.setRangeText(map[th.__lang[0]][lc][i], start, end+1);
                 th.setSelectionRange( start + 1, start + 1 );
             },10)
 
